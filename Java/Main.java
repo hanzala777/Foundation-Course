2b37de10c0bf7884cab3a545e89e0f6c66825763
Hanzala Ahmed <hanzhanzala@gmail.com>
2024-02-11 15:04:35 +0530

    foundation course

--- Java/Main.java
+++ Java/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static void main(String[] arg) {
+    public static void main (String[] arg) {
         Figure a = new Circle();
         Figure b = new Square();
         double areaCirle = a.area(2);
