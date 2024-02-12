b0adb2a7f26340f84321fc04595c67ba51d979f8
Hanzala Ahmed <hanzhanzala@gmail.com>
2024-02-12 14:05:07 +0530

    Encapsulation

--- Java/Main.java
+++ Java/Main.java
@@ -1,35 +1,59 @@
 public class Main{
     public static void main (String[] arg) {
-//        A a = new A() {};
-//        System.out.println(a.area(4));
-//        System.out.println(a.param(5));
-        Figure circle = new Circle();
-        System.out.println(circle.area(2));
-        System.out.println((circle.parameter(4)));
-        circle.m();
+        Students st1 = new Students();
+        st1.setId(2);
+        st1.setAge(0);
+        st1.setName("Hanzala");
+        st1.setMarks(99);
+        System.out.println("Id : " + st1.getId());
+        System.out.println("Age : " + st1.getAge());
+        System.out.println("Name : " + st1.getName()+ " " + st1.getLname());
+        System.out.println(st1.isPassed() ? "Pass" : "Fail");
     }
 }
-abstract class Figure{
-    abstract double area(int a);
-    abstract double parameter(int a);
-    public void m(){
-        System.out.println("Hello");
+class Students{
+    private int id, age;
+    private String name, lname = "Ahmed";
+
+    private int marks;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setMarks(int marks){
+        this.marks = marks;
+    }
+    public boolean isPassed(){
+        return marks > 40;
+    }
+
+
+    public void setAge(int age){
+        if(age <= 0) {
+            System.out.println("enter proper Age");
+            this.age = -1;
+        } else {
+            this.age = age;
+        }
+    }
+    public int getAge(){
+        return this.age;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return this.name;
+    }
+
+    public String getLname(){
+        return this.lname;
     }
 }
-class Circle extends Figure{
-    public double area(int a){
-        return 3.14*a*a;
-    }
-    public double parameter(int a){
-        return 2*3.14*a;
-    }
-}
-//interface A{
-//    default double area(int a){
-//        return a*a;
-//    }
-//    default double param(int a){
-//        return 4*a;
-//    }
-//
-//}
+
